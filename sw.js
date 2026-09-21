de95afc4b20ff22d0b12241d4057b9f60f974cab
DanielgFXch <gomesdani1999@gmail.com>
2026-09-21 11:57:43 +0100

    chore: refresh PWA shell cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='dgos-personal-shell-10';
+const CACHE='dgos-personal-shell-11';
 const ASSETS=['./','./index.html','./styles.css','./wow.css?v=0.37.1','./personal.css','./social.css','./personal.js','./social.js','./weather.js','./marketBrain.js','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png',
   './fonts/chakra-petch-500.woff2','./fonts/chakra-petch-600.woff2','./fonts/chakra-petch-700.woff2',
   './fonts/jetbrains-mono-400.woff2','./fonts/jetbrains-mono-500.woff2','./fonts/jetbrains-mono-700.woff2'];
